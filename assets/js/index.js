6c8b8503a28b3261b6d129dd1d63bd0fd6cffa3d
Inna <inna.kachinskaya13@gmail.com>
2021-08-02 19:08:08 +0300

    Fix index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,36 @@
 "use strict";
 class Worker {
-  constructor(firstname, lastname, days, salary, isPremium) {
+  constructor(firstName, lastName, days, salary, isPremium) {
+    if (isNaN(days) || typeof days !== "number") {
+      throw new TypeError("Days must be a number");
+    }
+    if (isNaN(salary) || typeof salary !== "number") {
+      throw new TypeError("Salary must be a number");
+    }
+    if (typeof firstName !== "string" || firstName.trim() === "") {
+      throw new TypeError("FirstName must be a string");
+    }
+    if (typeof lastName !== "string" || lastName.trim() === "") {
+      throw new TypeError("LastName must be a string");
+    }
+    if (typeof isPremium !== "boolean") {
+      throw new TypeError("Invalid typeof isPremium");
+    }
     if (salary < 0) {
       throw new RangeError("Salary must be a positive number");
     }
     if (days < 0) {
       throw new RangeError("Days must be a positive number");
     }
-    this.firstname = firstname;
-    this.lastname = lastname;
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.days = days;
     this.salary = salary;
     this.isPremium = isPremium;
   }
-  getFullName = () => `${this.firstname} ${this.lastname}`;
-  getSalary = (days) =>
-    this.isPremium
-      ? this.salary * this.days + this.salary * this.days * 0.25
-      : this.salary * this.days;
+  getFullName = () => `${this.firstName} ${this.lastName}`;
+  getSalary = () =>
+    this.isPremium ? this.salary * this.days * 1.25 : this.salary * this.days;
 }
 
 const worker1 = new Worker("Test", "Testovich", 30, 200, true);
